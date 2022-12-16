0c3c95d14852453e0bec4e54129a663213ea285d
guilherme.consultori <guilherme.consultori@wefin.com.br>
2022-12-16 18:34:02 -0300

    teste

---
+++ src/main/java/br/com/notes/Notes/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package br.com.notes.Notes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+
+import java.time.Duration;
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues();
+    }
+}
