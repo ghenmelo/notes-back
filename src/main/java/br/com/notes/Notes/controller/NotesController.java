d1c9d44d21c276a2aae716851236c79052426406
guilherme.consultori <guilherme.consultori@wefin.com.br>
2022-12-16 00:54:50 -0300

    cache redis

--- src/main/java/br/com/notes/Notes/controller/NotesController.java
+++ src/main/java/br/com/notes/Notes/controller/NotesController.java
@@ -3,6 +3,10 @@ package br.com.notes.Notes.controller;
 import br.com.notes.Notes.models.Note;
 import br.com.notes.Notes.repository.NotesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,22 +16,27 @@ import java.util.List;
 @ResponseStatus(HttpStatus.OK)
 @ResponseBody
 @CrossOrigin("*")
+@EnableCaching
 public class NotesController {
 
     @Autowired
     private NotesRepository notesRepository;
 
     @GetMapping
+    @Cacheable(cacheNames = "notes")
     public List<Note> getNotes() {
+        System.out.println("passou");
         return notesRepository.findAll();
     }
 
     @PostMapping
+    @CachePut(cacheNames = "notes", key = "#note.id")
     public Note saveNote(@RequestBody Note note) {
         return notesRepository.save(note);
     }
 
     @PutMapping("/{id}")
+    @CachePut(cacheNames = "notes", key = "#note.id")
     public Note putNote(@PathVariable(name = "id") Long id, @RequestBody Note note) {
         Note noteBanco = notesRepository
                 .findById(id)
@@ -40,6 +49,7 @@ public class NotesController {
     }
 
     @DeleteMapping("/{id}")
+    @CacheEvict(cacheNames = "notes", key = "#note.id")
     public void deleteNote(@PathVariable(name = "id") Long id) {
         notesRepository.deleteById(id);
     }
